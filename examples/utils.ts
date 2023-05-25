fdbb5fbf48713a4ac35627e40944bb70b36d7832
Adam Suskin <suskin@joincandidhealth.com>
2023-05-25 00:29:59 -0400

    remove unnecessary

--- examples/utils.ts
+++
@@ -1,9 +0,0 @@
-export function getEnvironmentVariable(key: string, fallback: string) {
-    let value = process.env[key];
-
-    if (!value || value.length === 0 || value.trim().length === 0) {
-        value = fallback;
-    }
-
-    return value;
-}
